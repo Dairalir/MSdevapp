86a544f3dfa4197572c98e6f886e18226193d0d7
Dairalir <daimanvm@gmail.com>
2022-12-16 09:05:30 +0100

    exo class

---
+++ src/Cours/Class/Employe.java
@@ -0,0 +1,13 @@
+package Cours.Class;
+
+public class Employe {
+    public String nom;
+    public float salaire;
+
+    public float augmenteSalaire()
+    {
+       // salaire = salaire + 500.0;
+        return salaire ;
+    }
+
+}
